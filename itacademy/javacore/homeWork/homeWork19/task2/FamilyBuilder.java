007af0211eb32f0210d4d5fdf1f5db54ce251456
Patotski-AS <7906241@gmail.com>
2021-01-31 12:34:29 +0300

    Добавил решение задачи 19_2

---
+++ itacademy/javacore/homeWork/homeWork19/task2/FamilyBuilder.java
@@ -0,0 +1,31 @@
+package com.itacademy.javacore.homeWork.homeWork19.task2;
+
+public class FamilyBuilder {
+    private Family family = new Family();
+
+    public FamilyBuilder addLastName(String lastName) {
+        family.setLastName(lastName);
+        return this;
+    }
+    public FamilyBuilder addMother(Person mother) {
+        family.setMother(mother);
+        return this;
+    }
+
+    public FamilyBuilder addFather(Person father) {
+        family.setFather(father);
+        return this;
+    }
+
+    public FamilyBuilder addChildren(Person child) {
+        family.setChildren(child);
+        return this;
+    }
+
+    public Family createFamily() {
+        Family newFamily = family;
+        family = null;
+        return newFamily;
+    }
+
+}
---
+++ itacademy/javacore/homeWork/homeWork19/task2/Main.java
@@ -0,0 +1,45 @@
+package com.itacademy.javacore.homeWork.homeWork19.task2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Создать класс Person с полями firstname, age.
+ * Создать класс Family c полями типа Person mother, father, список children, строка lastname.
+ * Создать список объектов класса Family и записать его в json. Название json файла,
+ * в который будет записываться список, передавать программе как параметр.
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<Family> families = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            families.add(new FamilyBuilder()
+                    .addLastName("Family" + i)
+                    .addFather(new Person("Father" + i, new Random().nextInt(20) + 18))
+                    .addMother(new Person("Mather" + i, new Random().nextInt(20) + 18))
+                    .addChildren(new Person("Child" + i, new Random().nextInt(5) + 5))
+                    .addChildren(new Person("Child" + i, new Random().nextInt(5) + 5))
+                    .createFamily());
+        }
+        File file = new File("D:\\JAVA\\JAVA\\src\\com\\itacademy\\javacore\\homeWork\\homeWork19\\task2\\families.json");
+        try {
+            jsonWriter(families,file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public static void jsonWriter(List<Family> families, File file) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        objectMapper.writeValue(file, families);
+    }
+
+}
